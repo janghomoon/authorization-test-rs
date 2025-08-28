fce0af2602e63ce26c64aed0a21b9b81205a452d
janghomoon <sonyjangho@gmail.com>
2025-08-28 15:14:00 +0900

    Initial commit

---
+++ src/main/java/kr/co/authorizationtestrs/AuthorizationTestRsApplication.java
@@ -0,0 +1,13 @@
+package kr.co.authorizationtestrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationTestRsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationTestRsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/authorizationtestrs/AuthorizationTestRsApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.authorizationtestrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationTestRsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
